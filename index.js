0b276c1b5aa1dbfb3d100d02075067bb21096c73
Jake Reck <jakereck@gmail.com>
2018-07-09 06:52:54 -0700

    Added a personal blog

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const   express = require('express'),
-        app     = express();
+const   express     = require('express'),
+        bodyParser  = require('body-parser'),
+        mongoose    = require('mongoose'),
+        methodOverride = require('method-override'),
+        app         = express();
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/portfolio_blog");
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
 
 app.get('/', (req,res) => {
     res.render('index');
@@ -21,6 +40,72 @@ app.get('/certificates', (req, res) => {
 
 app.get('/instafeed', (req, res) => {
     res.render('insta');
+});
+
+
+// BLOG //
+app.get('/blogs', (req, res) => {
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Error");
+        } else {
+            res.render("blogindex", {blogs: blogs});            
+        }
+    })
+});
+
+app.get('/blogs/new', (req, res) => {
+    res.render('newblog');
+});
+
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            res.render('newblog');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('showblog', {blog: foundBlog});
+        }
+    });
+});
+
+app.get('/blogs/:id/edit', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.render("editblog", {blog: foundBlog});
+        }
+    });
+});
+
+app.put('/blogs/:id', (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
+        }
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
 });
 
 app.listen(port, () => {
